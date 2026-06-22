4f7ae53f5b9958540afdb3179473122d6b29f5fb
catedraldesarrollorest <catedraldesarrollorest@gmail.com>
2026-06-22 13:53:51 -0400

    Add explicit SW registration with periodic update checks

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(reg => {
+    reg.update()
+    setInterval(() => reg.update(), 60 * 1000)
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
